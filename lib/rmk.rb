eae5fe424c78bd61c2db9ad61e82a786c0a5176c
pbl <3636470+pbl-pw@users.noreply.github.com>
2017-11-23 11:01:47 +0800

    incdir finished

--- lib/rmk.rb
+++ lib/rmk.rb
@@ -1,8 +1,10 @@
 require 'rmk/version'
 
 class Rmk
+	def self.normalize_path(path) path.gsub(?\\, ?/).sub(/^[a-z](?=:)/){|ch|ch.upcase} end
+
 	def initialize(srcroot, outroot)
-		@rootdir = Rmk::Dir.new srcroot, outroot
+		@rootdir = Rmk::Dir.new Rmk.normalize_path(srcroot), Rmk.normalize_path(outroot)
 		::Dir.mkdir outroot unless Dir.exist? outroot
 		@rootdir.parse
 	end
@@ -19,21 +21,29 @@ class Rmk::Rule
 end
 
 class Rmk::Dir
-	attr_reader :srcroot, :outroot, :path
-	attr_accessor :srcfiles, :outfiles
-	attr_accessor :vars, :rules, :subdirs
+	attr_reader :srcroot, :outroot, :path, :full_src_path, :full_out_path
+	attr_reader :srcfiles, :outfiles
+	attr_reader :vars, :rules, :subdirs
+	attr_writer :defaultfile
+	protected :defaultfile=
 
-	def initialize(srcroot, outroot, path = '', defaultfile = nil)
-		@srcroot, @outroot, @path, @defaultfile = srcroot, outroot, path, defaultfile
+	def initialize(srcroot, outroot, path = '')
+		@srcroot, @outroot, @path, @defaultfile = srcroot, outroot, path, nil
 		@vars, @rules, @subdirs = {}, {}, {}
 		@srcfiles, @outfiles = [], []
+		@full_src_path = ::File.join @srcroot, @path, ''
+		@full_out_path = ::File.join @outroot, @path, ''
 	end
 
-	def add_subdir(path) @subdirs[path] = Rmk::Dir.new @srcroot, @outroot, path, @defaultfile end
+	def add_subdir(path)
+		dir = @subdirs[path] = Rmk::Dir.new @srcroot, @outroot, path
+		dir.defaultfile = @defaultfile
+		dir
+	end
 
-	def join_src_path(file) ::File.join @srcroot, @path, file end
+	def join_src_path(file) ::File.join @full_src_path, file end
 
-	def join_out_path(file) ::File.join @outroot, @path, file end
+	def join_out_path(file) ::File.join @full_out_path, file end
 
 	def parse
 		file = join_src_path 'default.rmk'
@@ -90,10 +100,28 @@ class Rmk::Dir
 			dirs.gsub! /\s/, ?\n
 			dirs.gsub! ?\0, ' '
 			dirs = dirs.split /\n+/
+			raise "#{lid}; must have dir name or matcher" if dirs.empty?
+			dirs.each do |dir|
+				subs = ::Dir[::File.join @full_src_path, dir, '']
+				raise "#{lid}: subdir '#{dir}' doesn't exist" if subs.empty?
+				subs.each do |dir|
+					dir = dir.sub @full_src_path, ''
+					new_thread {add_subdir dir}
+				end
+			end
+			join_threads
 		else
 			match = /^\s*=\s*(?<value>.*)$/.match line
 			raise "#{lid} : ”Ô∑®¥ÌŒÛ" unless match
 			define_var firstword, match[:value], indent
 		end
 	end
+
+	# run in new thread
+	# @note not ready
+	def new_thread(&cmd) cmd.call end
+
+	# wait all threads
+	# @note not ready
+	def join_threads; end
 end
