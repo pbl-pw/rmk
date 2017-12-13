d8ce61ac97a76793c9ab2e2218d5cffcfac86cdf
pbl <3636470+pbl-pw@users.noreply.github.com>
2017-12-13 18:55:41 +0800

    simplify schedule

--- lib/rmk/schedule.rb
+++ lib/rmk/schedule.rb
@@ -1,29 +1,15 @@
 class Rmk; end
 
-class Rmk::Schedule
+module Rmk::Schedule
 	Thread.abort_on_exception = true
-	@thread_run_mutex = Thread::Mutex.new
-	@waitting_threads = []
-	@running_threads_cnt = 1
+	@queue = SizedQueue.new 8
 
-	class << self
-		private def default_thread_body(cmd)
-			Thread.stop unless @thread_run_mutex.synchronize do
-				next @running_threads_cnt += 1 unless @running_threads_cnt >= 8
-				@waitting_threads << Thread.current
-				false
-			end
+	def self.new_thread!(&cmd)
+		Thread.new do
+			@queue.push nil
 			result = cmd.call
-			@thread_run_mutex.synchronize do
-				if @waitting_threads.empty?
-					@running_threads_cnt -= 1 unless @running_threads_cnt <= 0
-				else
-					@waitting_threads.shift.run
-				end
-			end
+			@queue.pop
 			result
 		end
-
-		def new_thread!(&cmd) Thread.new cmd, &method(:default_thread_body) end
 	end
 end
