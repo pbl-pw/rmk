393db2b00203dd87c0dcc1ec9bb19ea2ea35f0a5
pbl <3636470+pbl-pw@users.noreply.github.com>
2017-12-29 19:31:58 +0800

    add rule rmk vars

--- lib/rmk/rule.rb
+++ lib/rmk/rule.rb
@@ -8,16 +8,21 @@ class Rmk::Rule
 	def initialize(command)
 		@command = command
 		@vars = {}
+		@rmk_vars = {'out'=>nil, 'collection'=>nil}
 	end
 	attr_reader :command
 
 	def vars; self end
 
-	def [](name) @vars[name]&.value end
+	def [](name) @rmk_vars[name] end
 
 	# add var define template
 	# @return Array<Var>
-	def []=(name, append = false, value) @vars[name] = Var.new(append, value) end
+	def []=(name, append = false, value)
+		return @vars[name] = Var.new(append, value) unless @rmk_vars.include? name
+		raise "special var '#{name}' can't be append" if append
+		@rmk_vars[name] = value
+	end
 
 	def apply_to(tgt)
 		@vars.each{|name, var| var.append? ? tgt[name] += var.value : tgt[name] = var.value }
