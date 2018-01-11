791fc3e0d3c30be00ac614f1e3e852ad32f3dd04
pbl <3636470+pbl-pw@users.noreply.github.com>
2018-01-11 08:31:18 +0800

    fix thread deadlock error, not limit max thread count until use a thread pool

--- lib/rmk/schedule.rb
+++ lib/rmk/schedule.rb
@@ -2,14 +2,8 @@ class Rmk; end
 
 module Rmk::Schedule
 	Thread.abort_on_exception = true
-	@queue = SizedQueue.new 8
 
 	def self.new_thread!(&cmd)
-		Thread.new do
-			@queue.push nil
-			result = cmd.call
-			@queue.pop
-			result
-		end
+		Thread.new &cmd
 	end
 end
